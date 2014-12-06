202ba12ed84953f00faf860316e921f2e39ebf76
reneeb <github@renee-baecker.de>
2014-12-06 09:47:24 +0100

    prepare next release

--- lib/Mojolicious/Plugin/AdditionalValidationChecks.pm
+++ lib/Mojolicious/Plugin/AdditionalValidationChecks.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::AdditionalValidationChecks;
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Email::Valid;
 use Scalar::Util qw(looks_like_number);
